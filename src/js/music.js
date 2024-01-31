380b1fc4e1fef29c6ef07c4c433b2a8f4a66ad4a
YushMC <mujushaxb10@gmail.com>
2024-01-31 10:03:43 -0600

    Fixed frame spotify url

--- src/js/music.js
+++ src/js/music.js
@@ -1,5 +1,5 @@
 const URL_list = "https://open.spotify.com/embed/"
-const defaultURL_list = "https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO0F0wVy?utm_source=generator";
+const defaultURL_list = "playlist/37i9dQZF1DZ06evO0F0wVy";
 const finaly_list = "?utm_source=generator";
 const btnSaveUrl_list = document.querySelector('#save_url_list');
 const btnReset_list = document.querySelector('#reset_playlist');
@@ -8,14 +8,14 @@ const btnClear_list = document.querySelector('#clear_input_playlist');
 
 const setURL_List = (url) => {
     localStorage.setItem('url_playlist', url);
+    let url_lista = URL_list + url + finaly_list;
     document.getElementById("playlist_frame").style.display="block";
-    document.getElementById("playlist_frame").setAttribute("src", url);
+    document.getElementById("playlist_frame").setAttribute("src", url_lista);
 }
 
 btnSaveUrl_list.addEventListener('click', ()  => {
     let datos = document.getElementById('playlist').value;
-    let url_lista = URL_list + datos + finaly_list;
-    setURL_List(url_lista);
+    setURL_List(datos);
     window.location.reload();
 });
 
